17e5c14e1a3cb3a46fc60790d6824c66531b38b4
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2021-10-05 14:06:01 -0300

    add exact na home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink className="nav-link" to="#">Home</NavLink>
+              <NavLink exact className="nav-link" to="#">Home</NavLink>
             </li>
             <li className="nav-item">
               <NavLink className="nav-link" to="/pesquisa">Pesquisa</NavLink>
